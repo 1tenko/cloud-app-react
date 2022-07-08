995ff4c913597a6690a33af62c5816baac04159e
tenko <tajm.ogunbiyi@gmail.com>
2022-07-08 15:13:29 +0100

    :sparkles: Dropdown nav bar implemented for smaller screens / mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Nav = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
@@ -22,10 +25,24 @@ const Nav = () => {
           </button>
           <button className="px-8 py-3">Sign Up</button>
         </div>
-        <div className="md:hidden">
-          <MenuIcon className="w-5" />
+        <div className="md:hidden" onClick={handleClick}>
+          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
       </div>
+
+      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+        <li className="border-b-2 border-zinc-300 w-full">Home</li>
+        <li className="border-b-2 border-zinc-300 w-full">About</li>
+        <li className="border-b-2 border-zinc-300 w-full">Support</li>
+        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
+        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+        <div className="flex flex-col my-4">
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+      </ul>
     </div>
   );
 };
